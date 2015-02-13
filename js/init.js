ed646e39d5f438997cf6a34b98c9a482dc733a67
John Fragkoulis <john.fragkoulis@gmail.com>
2015-02-13 11:40:37 +0200

    Updated site

--- js/init.js
+++ js/init.js
@@ -112,7 +112,7 @@
 					overlayOpacity: 0.85,
 					popupCloserText: '',
 					popupLoaderText: '',
-					selector: '.work-item a',
+					selector: '.work-item a.image',
 					usePopupCaption: true,
 					usePopupDefaultStyling: false,
 					usePopupEasyClose: false,
